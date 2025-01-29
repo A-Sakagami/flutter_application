c454a9445604e76aea64b76d162fae946ca25c63
A-Sakagami <sakagami@genz.jp>
2025-01-29 17:14:45 +0900

    VDでアプリ起動成功

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,15 @@ void main() {
   runApp(const MyApp());
 }
 
+// コンストラクタ
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
+  // 最初に呼ばれる関数 @Overrideなので必須
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'workflow Tool',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -31,7 +32,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Workflow Tool'),
     );
   }
 }
